5169517e80b80bd6c37e869e49eb734b1f883240
Anna Levkovich <7604442@gmail.com>
2023-02-09 21:59:02 +0300

    fix: to do part work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = true;
+  for (let i = 0; i < str; i++) {
+    bracketsConfig.forEach(element => {
+      if (element.includes(str[i])) {
+        result = true;
+      } else {
+        result = false;
+      }
+    });
+  }
+  return result
 }
